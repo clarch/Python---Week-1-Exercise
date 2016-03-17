9fb3c34446512ba87d9a1260df8eb6bc0b496912
Kclarch <clarine.kirui@gmail.com>
2016-03-17 18:29:25 +0300

    Sum_ofUnique_Digits

---
+++ Sum_of_Unique_Digits.py
@@ -0,0 +1,12 @@
+def sum_of_digits(A):
+	sum = 0
+	for a in A:
+		B = list(map(int, str(a)))
+		for b in B:
+			c = []
+			if b not in c
+				c.append(b)
+		sum += b
+	print sum
+
+sum_of_digits([12, 34, 56, 78])
